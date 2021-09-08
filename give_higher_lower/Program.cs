ee604de1ea60e38ab59b132e00567975eb720647
ornella olivastri <oolivastri@frba.utn.edu.ar>
2021-09-08 16:49:04 -0300

    high project

---
+++ give_higher_lower/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace give_higher_lower
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int limite = 10, numero = 1;
+
+            while (numero <= limite)
+            {
+                if(numero % 2 == 0)
+                {
+                    Console.WriteLine(numero + " es par");
+
+                }else if(numero % 2 == 1)
+                {
+                    Console.WriteLine(numero + " es impar");
+                }
+                numero++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
