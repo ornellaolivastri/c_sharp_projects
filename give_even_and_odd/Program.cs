e19ab1ea1e99df0cf9497ef41f2e812b7653806c
ornella olivastri <oolivastri@frba.utn.edu.ar>
2021-09-08 17:21:46 -0300

    give even and odd project

---
+++ give_even_and_odd/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace give_even_and_odd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
